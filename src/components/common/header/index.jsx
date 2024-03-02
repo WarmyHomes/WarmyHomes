c21b8be9be017110d380eceba209d6d71ac7d297
Aliye ilbay <yazm921@gmail.com>
2024-03-02 20:03:29 +0100

    header

--- src/components/common/header/index.jsx
+++ src/components/common/header/index.jsx
@@ -1,12 +1,49 @@
-import React from 'react'
-import Menubar from './menubar'
+import Image from "next/image";
+import "./Header.scss";
+import Link from "next/link";
 
-const Header = () => {
+export default  function Header() {
   return (
-    <>
-      <Menubar/>
-    </>
-  )
+    <header className="header-main-container">
+      <div className="header-left-container">
+        <Link href={"/"}>
+          <Image width={210} height={50} src={"/images/logo/logo.png"} />
+        </Link>
+        <nav className="header-nav">
+          <ul>
+            <li>
+              <Link href={"/"}>Home</Link>
+            </li>
+            <li>
+              <Link href={"/"}>Properties</Link>
+            </li>
+            <li>
+              <Link href={"/"}>About</Link>
+            </li>
+            <li>
+              <Link href={"/"}>Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="header-right-container">
+        <div className="login-signup-buttons">
+          <i className="bi bi-person"></i>
+          <button>
+            <Link href={"/"}>Login</Link>
+          </button>
+          <span>/</span>
+          <button>
+            <Link href={"/"}>Register</Link>
+          </button>
+        </div>
+        <button className="add-property-button">
+          <Link href={"/"}>
+            Add Property
+            <i className="bi bi-arrow-right"></i>
+          </Link>
+        </button>
+      </div>
+    </header>
+  );
 }
-
-export default Header
