7c772c1641bd7f19613f9408507a7eea4310cf4a
Jamshaid <yazm921@gmail.com>
2024-03-05 00:29:37 +0500

    header-complete, added spacer, add icons folder in public for custom svgs

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,28 +1,22 @@
 import Footer from "@/components/common/footer";
 import Menubar from "@/components/common/header/menubar";
-import Spacer from "@/components/common/spacer";
+import Spacer from "@/components/common/misc/spacer";
 import ContactUs from "@/components/home-page/contact-us";
 import RegisterNow from "@/components/home-page/register-now";
 import { config } from "@/helpers/config";
 
 export const metadata = {
-	title: config.project.slogan,
+  title: config.project.slogan,
 };
-export default function Home(){
-  return(
-    
-
-     
+export default function Home() {
+  return (
     <>
-   <Menubar/>
-   <Spacer/>
-   <RegisterNow/>
-   <Spacer/>
-   <ContactUs/>
-   <Spacer/>
-  
-      </>
-
-    
+      <Menubar />
+      <Spacer />
+      <RegisterNow />
+      <Spacer />
+      <ContactUs />
+      <Spacer />
+    </>
   );
 }
