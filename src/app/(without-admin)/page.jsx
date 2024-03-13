7999e11cccbcc0037af8c28dc3fef0d872a87eb8
hasan25hasan <132673382+hasan25hasan@users.noreply.github.com>
2024-03-14 00:31:33 +0300

    has

--- src/app/(without-admin)/page.jsx
+++ src/app/(without-admin)/page.jsx
@@ -5,12 +5,7 @@ import ContactUs from "@/components/home-page/contact-us";
 import ExploreByCities from "@/components/home-page/explore-cities";
 import ExploreByTypes from "@/components/home-page/explore-type";
 import RegisterNow from "@/components/home-page/register-now";
-<<<<<<< HEAD:src/app/page.jsx
-import Slider from "@/components/home-page/hero-section";
-=======
-import Slider from "@/components/home-page/slider";
 import SellingOption from "@/components/home-page/selling-option";
->>>>>>> main:src/app/(without-admin)/page.jsx
 import { config } from "@/helpers/config";
 import HeroSection from "@/components/home-page/hero-section";
 
