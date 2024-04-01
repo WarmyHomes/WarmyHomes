5c247d02f90bfc732b4a1efca46da266841cd79f
hocabilgic <hocabilgic@gmail.com>
2024-04-01 17:46:00 +0300

    advertType

---
+++ src/app/admin/advert-types/page.jsx
@@ -0,0 +1,14 @@
+// Page.jsx
+
+import React from 'react';
+import AdvertTypeFrom from '@/components/admin/advert-types/advert-types-from';
+
+const AdvertType = () => {
+  return (
+          <>
+          <AdvertTypeFrom/>
+          </>
+  );
+};
+
+export default AdvertType;
