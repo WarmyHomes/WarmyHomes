89a214e6f09c93540ab48fa7df25f8e1d7cf23d9
AliyeZM <yazm921@gmail.com>
2024-04-05 01:51:54 +0500

    image showcase and description in adverts detail page

--- src/app/(without-admin)/adverts/details/page.jsx
+++ src/app/(without-admin)/adverts/details/page.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import "./page.scss";
 import { MdOutlineLocationOn } from "react-icons/md";
 import { CgTag } from "react-icons/cg";
@@ -6,6 +8,19 @@ import { FaRegClock } from "react-icons/fa";
 import { FaRegEye } from "react-icons/fa6";
 
 const AdvertsDetailsPage = () => {
+  const images = [
+    "/images/about.png",
+    "/images/highlight.png",
+    "/images/selling-option.png",
+    "/images/about.png",
+  ];
+
+  const [active, setActive] = useState(images[0]);
+
+  const onImageClick = (el) => {
+    setActive(el);
+  };
+
   return (
     <div className="adverts-details-page-container">
       <div className="advert-detail-page-header">
@@ -33,6 +48,39 @@ const AdvertsDetailsPage = () => {
         <div className="advert-detail-header-right">
           <h1>$1500.00</h1>
         </div>
+      </div>
+      <div className="advert-detail-page-main">
+        <div className="advert-detail-page-main-left">
+          <div className="image-schowcase-container">
+            <img src={active} alt="" className="main-image" />
+            <div className="images-container">
+              {images.map((el) => (
+                <img
+                  onClick={() => onImageClick(el)}
+                  src={el}
+                  alt=""
+                  className="main-image"
+                />
+              ))}
+            </div>
+          </div>
+          <div className="description-container">
+            <h5>Description</h5>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum
+            </p>
+          </div>
+        </div>
+        <div className="advert-detail-page-main-right"></div>
       </div>
     </div>
   );
