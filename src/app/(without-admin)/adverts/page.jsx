d9f1ac36bdf93431d51f719baf0e385ba6f5a034
fevzibaykus <fevzibaykus@gmail.com>
2024-04-29 23:27:59 +0300

    Fevzi

--- src/app/(without-admin)/adverts/page.jsx
+++ src/app/(without-admin)/adverts/page.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./page.scss";
 import PageHeader from "@/components/common/page-header";
 import Spacer from "@/components/common/misc/spacer";
+import PropertyCard from "@/components/common/property-card";
 
 const AdvertPage = () => {
   return <>
   <PageHeader title={"PROPERTIES"}/>
   <Spacer/>
+  
   
   </>;
 };
