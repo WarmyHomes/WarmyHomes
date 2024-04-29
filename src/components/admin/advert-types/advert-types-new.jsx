24e397aa6af750f40cb9dbbabc6341ba4108f473
hocabilgic <hocabilgic@gmail.com>
2024-04-30 01:27:55 +0300

    advertTypeEdit

--- src/components/admin/advert-types/advert-types-new.jsx
+++ src/components/admin/advert-types/advert-types-new.jsx
@@ -1,21 +1,23 @@
-"use client";
-
-import SubmitButton from "@/components/common/form-fields/submit-button";
+"use client"
+import React, { useState } from 'react';
+import "./advert-type-from.scss"
+import { createAdvertTypeAction } from '@/actions/advert-type-action';
 import { initialResponse, isInvalid } from "@/helpers/form-validation";
-
-import { updateAdvertType } from '@/services/advertType-servise';
-import CancelButton from "@/components/common/form-fields/cancel-button";
+import SubmitButton from "@/components/common/form-fields/submit-button";
 import { useFormState } from "react-dom";
-const AdvertTypeEdit = ({ data }) => {
-	
+
+
+// Yeni reklam türü oluşturma bileşeni
+const AdvertTypeNew = () => {
 
     // Form durumunu ve işlemlerini yönetmek için useFormState kancını kullanıyoruz
+    // Bu noktada updateAdvertType ve initialResponse'un tanımlanması gerekiyor
+
+    // State ve dispatch fonksiyonunu kullanarak form durumunu yönetiyoruz
     const [state, dispatch] = useFormState(
-        updateAdvertType, // form gönderme işlemlerini yapan eylem (action)
-        initialResponse // form durumunun başlangıç değeri
+      createAdvertTypeAction, // Form gönderme işlemlerini yapan eylem (action)
+        initialResponse // Form durumunun başlangıç değeri
     );
-
-
 
     return (
         <div className="container">
@@ -23,14 +25,15 @@ const AdvertTypeEdit = ({ data }) => {
                 <div className="card-body">
                     <div className="card-title">Edit</div>
 
+                    {/* Hata mesajı varsa göster */}
                     {state?.message ? (
                         <div className="alert alert-danger">
                             {state.message}
                         </div>
                     ) : null}
 
-                    <form action={dispatch} noValidate>
-                        <input type="hidden" name="userId" value={data.id} />
+                    <form action={dispatch} noValidate> {/* Form gönderildiğinde dispatch fonksiyonunu çağırır */}
+                        <input type="hidden" name="userId"  /> {/* Kullanıcı kimliğini gizli bir şekilde gönderir */}
                         <div className="row row-cols-1 row-cols-md-2 row-cols-xl-3">
                             <div className="col">
                                 <div className="form-floating mb-3">
@@ -42,9 +45,9 @@ const AdvertTypeEdit = ({ data }) => {
                                         id="title"
                                         name="title"
                                         placeholder="title"
-                                        defaultValue={data.title}
+                                       
                                     />
-                                    <label htmlFor="title">First Name</label>
+                                    <label htmlFor="title">Title</label>
                                     <div className="invalid-feedback">
                                         {state.errors?.name}
                                     </div>
@@ -53,8 +56,8 @@ const AdvertTypeEdit = ({ data }) => {
                         </div>
 
                         <div className="d-flex align-items-center justify-content-center gap-3">
-                            <CancelButton />
-                            <SubmitButton title="Update" />
+                          
+                            <SubmitButton title="Create" />
                         </div>
                     </form>
 
@@ -64,4 +67,4 @@ const AdvertTypeEdit = ({ data }) => {
     );
 };
 
-export default AdvertTypeEdit;
+export default AdvertTypeNew; // Bileşeni dışa aktarır
