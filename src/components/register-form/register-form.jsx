6b80dcc19d9f5f606fcf1fb069f7407401bd792b
ZeynepDmr <zeynepnevadmr@gmail.com>
2024-05-06 21:30:31 +0300

    register-güncellendi

--- src/components/register-form/register-form.jsx
+++ src/components/register-form/register-form.jsx
@@ -1,15 +1,15 @@
 "use client";
 
-import Link from "next/link";
-import "./register-form.scss";
-import { initialResponse} from "@/helpers/form-validation";
-import { useFormState } from "react-dom";
-import SubmitButton from "../common/form-fields/submit-button";
 import { createRegisterAction } from "@/actions/user-actions";
+import Link from "next/link";
+import { useFormState } from "react-dom";
+import "./register-form.scss";
+import SubmitButton from "../common/form-fields/submit-button";
+import { initialResponse, isInvalid } from "@/helpers/form-validation";
+import InputMask from "react-input-mask-next";
 
 const RegisterForm = () => {
 	const [state, dispatch] = useFormState(createRegisterAction, initialResponse);
-
 
 	return (
 		<div className="container register-form">
@@ -18,119 +18,108 @@ const RegisterForm = () => {
 					<div className="card">
 						<div className="card-body">
 							<form action={dispatch} noValidate>
-								<div
-									className={`form-floating mb-3 `}
-								>
-									<input
-										type="text"
-										className="form-control"
-										id="firstname"
-										name="firstname"
-										
-										
-									/>
-									<label htmlFor="firstname">
-                                    First Name
-									</label>
-									
+                                <div className="form-floating mb-3">
+                                    <input
+                                        type="text"
+                                        className={`form-control ${isInvalid(
+											state.errors?.firstname
+										)}`}
+                                        id="firstname"
+                                        name="firstname"
+                                        placeholder="First Name"
+                                    />
+                                    <label htmlFor="firstname">First Name</label>
+									<div className="invalid-feedback">
+										{state.errors?.firstname}
+									</div>
+                                </div>
+                                <div className="form-floating mb-3">
+                                    <input
+                                        type="text"
+                                        className={`form-control ${isInvalid(
+											state.errors?.lastname
+										)}`}
+                                        id="lastname"
+                                        name="lastname"
+										placeholder="Last Name"
+                                    />
+                                    <label htmlFor="lastname">Last Name</label>
+									<div className="invalid-feedback">
+										{state.errors?.lastname}
+									</div>
+                                </div>
+                                <div className="form-floating mb-3">
+                                    <InputMask
+									className={`form-control ${isInvalid(
+											state.errors?.phone
+										)}`}
+                                        id="phone"
+                                        name="phone"
+										placeholder="Phone number"
+										mask="999-999-9999"
+                                        
+                                    />
+                                    <label htmlFor="phone">Phone</label>
+									<div className="invalid-feedback">
+										{state.errors?.phone}
+									</div>
+                                </div>
+                                <div className="form-floating mb-3">
+                                    <input
+                                        type="text"
+                                        className={`form-control ${isInvalid(
+											state.errors?.email
+										)}`}
+                                        id="email"
+                                        name="email"
+                                        placeholder="Email"
+                                    />
+                                    <label htmlFor="email">Email</label>
+									<div className="invalid-feedback">
+										{state.errors?.email}
+									</div>
+                                </div>
+                                <div className="form-floating mb-3">
+                                    <input
+                                        type="password"
+                                        className={`form-control ${isInvalid(
+											state.errors?.password
+										)}`}
+                                        id="password"
+                                        name="password"
+										placeholder="Password"
+                                    />
+                                    <label htmlFor="password">Password</label>
+									<div className="invalid-feedback">
+										{state.errors?.password}
+									</div>
+                                </div>
+                                <div className="form-floating mb-3">
+                                    <input
+                                        type="password"
+										className={`form-control ${isInvalid(
+											state.errors?.confirmPassword
+										)}`}
+                                        id="confirmPassword"
+                                        name="confirmPassword"
+                                        placeholder="Confirm password"
+                                    />
+                                    <label htmlFor="confirmPassword">Confirm Password</label>
+									<div className="invalid-feedback">
+										{state.errors?.confirmPassword}
+									</div>
+                                </div>
+								<div>
+                                <SubmitButton title="Register" />
+                                <h6>If you already have an account. <Link href="/login">Login now!</Link>{" "}</h6>
 								</div>
-                                <div
-									className={`form-floating mb-3 `}
-								>
-									<input
-										type="text"
-										className="form-control"
-										id="lastname"
-										name="lastname"
-										
-										
-									/>
-									<label htmlFor="lastname">
-                                    Last Name
-									</label>
-									
-								</div>
-
-								<div
-									className={`form-floating mb-3 `}
-								>
-									<input
-										type="text"
-										className="form-control"
-										id="phone"
-										name="phone"
-										
-										
-									/>
-									<label htmlFor="phone">
-                                    Phone
-									</label>
-									
-								</div>
-
-								
-                                <div
-									className={`form-floating mb-3 `}
-								>
-									<input
-										type="text"
-										className="form-control"
-										id="email"
-										name="email"
-										
-										
-									/>
-									<label htmlFor="email">
-                                    Email
-									</label>
-									
-								</div>
-
-                                <div
-									className={`form-floating mb-3 `}
-								>
-									<input
-										type="text"
-										className="form-control"
-										id="Password"
-										name="Password"
-										
-										
-									/>
-									<label htmlFor="Password">
-                                    Password
-									</label>
-									
-								</div>
-
-                                <div
-									className={`form-floating mb-3 `}
-								>
-									<input
-										type="text"
-										className="form-control"
-										id="Confirm Password"
-										name="Confirm Password"
-										
-										
-									/>
-									<label htmlFor="Confirm Password">
-                                    Confirm Password
-									</label>
-									
-								</div>
-                                
-
-                              
-								<SubmitButton title="Register" />
-								<h6>If you already have an account. <Link href="/login">Login now!</Link> </h6>
-							</form>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default RegisterForm;
