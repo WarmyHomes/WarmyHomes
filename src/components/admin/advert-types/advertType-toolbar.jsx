da4b3e6c199311e25a303a17162fa449a0fac13b
AliyeZM <yazm921@gmail.com>
2024-05-12 01:20:06 +0500

    Changed the table design, advert type design page

--- src/components/admin/advert-types/advertType-toolbar.jsx
+++ src/components/admin/advert-types/advertType-toolbar.jsx
@@ -3,48 +3,32 @@ import { deleteAdvertTypeAction } from "@/actions/advert-type-action";
 import { swalAlert, swalConfirm } from "@/helpers/swal";
 import Link from "next/link";
 import React from "react";
-import { TfiPencil, TfiTrash } from "react-icons/tfi";
+import { TfiPencil } from "react-icons/tfi";
+import { AiOutlineDelete } from "react-icons/ai";
 
 const AdvertTypeToolbar = ({ row }) => {
+  const { id } = row;
 
+  const handleDelete = async () => {
+    const res = await swalConfirm("Are you sure to delete");
+    if (!res.isConfirmed) return;
 
-	const { id } = row;
+    try {
+      const res = await deleteAdvertTypeAction(id);
+    } catch (err) {
+      console.log(err);
+      swalAlert(err.message, "error");
+    }
+  };
 
-	
-
-	const handleDelete = async () => {
-		const res = await swalConfirm("Are you sure to delete");
-		if (!res.isConfirmed) return;
-
-		try {
-			const res = await deleteAdvertTypeAction(id);
-		} catch (err) {
-			console.log(err);
-			swalAlert(err.message, "error");
-		}
-	};
-
-	
-
-	return (
-		<div>
-			<Link
-				type="button"
-				className="btn btn-link btn-dark"
-				href={`/admin/advert-types/${id}`}
-			>
-				<TfiPencil />
-			</Link>
-
-			<button
-				type="button"
-				className="btn btn-link btn-dark"
-				onClick={handleDelete}
-			>
-				<TfiTrash />
-			</button>
-		</div>
-	);
+  return (
+    <div className="table-actions-container">
+      <Link href={`/admin/advert-types/${id}`}>
+        <TfiPencil className="action-icon-color" />
+      </Link>
+      <AiOutlineDelete onClick={handleDelete} className="action-icon-color" />
+    </div>
+  );
 };
 
 export default AdvertTypeToolbar;
