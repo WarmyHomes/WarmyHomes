beeb11ff1739e9b3d3a500e8a4b83531fec2d4f6
hocabilgic <hocabilgic@gmail.com>
2024-05-12 12:29:55 +0300

    contact message

--- src/actions/contact-us-queries-actions.js
+++ src/actions/contact-us-queries-actions.js
@@ -11,38 +11,40 @@ import { redirect } from "next/navigation";
 import * as Yup from "yup";
 
 const FormSchema = Yup.object({
-  firstName: Yup.string().required("Required"),
-  lastName: Yup.string().required("Required"),
-  email: Yup.string().email("Invalid email").required("Required"),
-  message: Yup.string().required("Required"),
-});
+	first_name: Yup.string().required("Required"),
+	last_name: Yup.string().required("Required"),
+	email: Yup.string().email("Invalid email").required("Required"),
+	message: Yup.string().required("Required"),
+  });
+  
 
 export const createContactUsQueryAction = async (prevState, formData) => {
-  const required = ["firstName", "lastName", "email", "message"];
-  try {
-    const form = new FormData();
-    required.forEach((key) => form.append(key, formData.get(key)));
-
-    const fields = convertFormDataToJson(form);
-    console.log(fields, "fileds");
-
-    FormSchema.validateSync(fields, { abortEarly: false });
-
-    const res = await createNewContactUsQuery(fields);
-    const data = await res.json();
-
-    if (!res.ok) {
-      return response(false, "", data?.validations);
-    }
-
-    return response(true, "Query saved successfully!", {});
-  } catch (err) {
-    if (err instanceof Yup.ValidationError) {
-      return getYupErrors(err.inner);
-    }
-    console.log(err, "erro");
-  }
-};
+	const required = ["first_name", "last_name", "email", "message"];
+	try {
+	  const form = new FormData();
+	  required.forEach((key) => form.append(key, formData.get(key)));
+  
+	  const fields = convertFormDataToJson(form);
+	  console.log(fields, "fileds");
+  
+	  FormSchema.validateSync(fields, { abortEarly: false });
+  
+	  const res = await createNewContactUsQuery(fields);
+	  const data = await res.json();
+  
+	  if (!res.ok) {
+		return response(false, "", data?.validations);
+	  }
+  
+	  return response(true, "Query saved successfully!", {});
+	} catch (err) {
+	  if (err instanceof Yup.ValidationError) {
+		return getYupErrors(err.inner);
+	  }
+	  console.log(err, "erro");
+	}
+  };
+  
 
 export const deleteContactMessageAction = async (id) => {
 	if (!id) throw new Error("id is missing");
