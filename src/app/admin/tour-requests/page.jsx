6279ca221579985ba9756136f561f39bc185fa7a
Emre Akkaş <akkasemre.0021@gmail.com>
2024-05-16 23:38:18 +0300

    tour-req-admin-details

--- src/app/admin/tour-requests/page.jsx
+++ src/app/admin/tour-requests/page.jsx
@@ -1,21 +1,18 @@
-
-import AdminAllTourRequest from '@/components/admin/tour-request/adminAllTourRequests'
+import AdminTourRequests from '@/components/admin/admin-tour-request/admin-tour-request';
 import Spacer from '@/components/common/misc/spacer'
-import PageHeader from '@/components/common/page-header'
-import { getAllTourRequestWithPage } from '@/services/admin-tour-request-details-service'
+import { getAllTourRequestWithPage } from '@/services/admin-tour-request-details-service';
+
 import React from 'react'
 
-const page = async({searchParams}) => {
-  const { page } = searchParams;
-
-	const res = await getAllAdminsByPage(page);
-	const data = await res.json();
-	if (!res.ok) throw new Error(data.message);
-  console.log("data: ",data);
+const page = async({ searchParams}) => {
+  const {page} = searchParams;
+  const res = await getAllTourRequestWithPage(page);
+  const data = await res.json();
+  console.log("DATA",data);
+  if(!res.ok) throw new Error(data.message);
   return (
     <>
-      <Spacer height={5}></Spacer>
-      <AdminAllTourRequest data={data}></AdminAllTourRequest>
+      <AdminTourRequests data={data}/>
     </>
     
   )
--- src/services/admin-tour-request-details-service.js
+++ src/services/admin-tour-request-details-service.js
@@ -1,22 +1,41 @@
 import {getAuthHeader} from "@/helpers/auth";
 import {config} from "@/helpers/config";
+import { convertFormDataToJson } from "@/helpers/form-validation";
 
 const API_URL = config.api.baseUrl;
 
 export const getTourRequestWithId = async (id) => {
+  const header = await getAuthHeader();
   return fetch (`${API_URL}/tour-request/${id}/auth`,{
-    method:"get",
-    body: await getAuthHeader(),
+    method:"GET",
+    headers: header,
   });
 };
 
+export const deleteTourRequestWithId = async (id) => {
+  try{
+    const response = await fetch(`${API_URL}/tour-requests/${id}`,{
+      method: "DELETE",
+      headers: await getAuthHeader(),
+    });
+    if(!response.ok){
+      const errorData = await response.json();
+      throw new Error(errorData.message|| 'Error occurred');
+    }
+    return response.json();
+  }catch(error){
+    console.error("API Error", error);
+    throw new Error(error.message|| 'Error occurred while deleting the tour request');
+  }
+};
 
-export const deleteTourRequestWithId = async(id) => {
-  return fetch(`${API_URL}/tour-request/${id}`,{
-    method: "delete",
+export const updateTourRequest = async( payload ) => {
+  return fetch (`${API_URL}/tour-requests/${payload.id}/auth`,{
+    method: "PUT",
     headers: await getAuthHeader(),
+    body: JSON.stringify(payload)
   });
-};
+}; 
 
 export const getAllTourRequestWithPage = async(
   page = 0,
@@ -24,7 +43,7 @@ export const getAllTourRequestWithPage = async(
 	sort = "tour_date",
 	type = "asc"
 ) => {
-    const qs = `page=${page}&size=${size}&sort=${sort}&type=${type}`;
+    const qs = `?page=${page}&size=${size}&sort=${sort}&type=${type}`; // Sorgu dizisini oluşturun
     return fetch(`${API_URL}/tour-requests/admin${qs}`, {
       headers: await getAuthHeader(),
     });
