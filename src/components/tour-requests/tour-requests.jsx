41a54d15c2ce4c8bc1042dab18a45c28b1c40264
Emre Akkaş <akkasemre.0021@gmail.com>
2024-05-19 01:01:56 +0300

    user tr-trd-trıd

--- src/components/tour-requests/tour-requests.jsx
+++ src/components/tour-requests/tour-requests.jsx
@@ -1,28 +1,18 @@
 "use client"
 import DataTable, { Column } from "@/components/common/form-fields/data-table";
 import React from "react";
-import { RiSearch2Line } from "react-icons/ri";
-import TourRequestsToolBar from "../admin/admin-tour-request/tour-request-toolbar";
+import UserTourRequestToolBar from "./tour-requests-tool-bar";
+
 
 
 
 const TourRequests = ({ data }) => {
   const {content, totalPages, number, size} = data;
   const handleToolbar = (row) => {
-    return <TourRequestsToolBar row={row} />
+    return <UserTourRequestToolBar row={row} />
   }
   return (
     <div className="tour-requests-conitainer mt-3">
-      
-      <div className="list-filters-main-container">
-        <div className="search-input-container">
-          <input type="text" placeholder="Type Something" />
-          <div className="search-icon-container">
-            <RiSearch2Line className="icon" />
-          </div>
-        </div>
-      </div>
-
       <DataTable title={``} dataSource={data} dataKey="id" pagination={false}>
         <Column title="property" field = "advert_id"/>
         <Column />
@@ -33,7 +23,10 @@ const TourRequests = ({ data }) => {
         <Column title="Tour Date" field = "tour_date"/>
         <Column title="Tour Time" field = "tour_time"/>
         <Column title="Actions" template={handleToolbar} />
+
+        
       </DataTable>
+      
       
     </div>
   );
--- src/services/tour-requests-service.js
+++ src/services/tour-requests-service.js
@@ -15,5 +15,40 @@ export const getUsersAllTourRequests = async (
     headers : await getAuthHeader(),
   }) 
 };
+
+export const deleteUsersTourRequest = async (id) =>{
+  try{
+    const response = await fetch(`${API_URL}/tour-requests/${id}`,{
+      method: "DELETE",
+      headers: await getAuthHeader(),
+    });
+    if(!response.ok){
+      const errorData = await response.json();
+      throw new Error(errorData.message|| 'Error occurred');
+    }
+    return response.json();
+  }catch(error){
+    console.error("API Error", error);
+    throw new Error(error.message|| 'Error occurred while deleting the tour request');
+  }
+}
+
+export const getUsersTourRequestDetails = async (id) =>{
+  try {
+    const response = await fetch(`${API_URL}/tour-requests/${id}/auth`, {
+      method: "GET",
+      headers: await getAuthHeader(),
+    });
+
+    if (!response.ok) {
+      throw new Error('Error occurred while fetching tour details.');
+    }
+
+    return response; 
+  } catch (err) {
+    console.error("API ERROR", err);
+    throw new Error('An error occurred while fetching tour details. Please try again later.');
+  }
+}
   
 
