b7771b0457fba5e48b3e3420fd29e748319e2bab
ZeynepDmr <zeynepnevadmr@gmail.com>
2024-06-05 23:50:24 +0300

    discover-properties-update

--- src/components/home-page/discover-properties/discover-properties.jsx
+++ src/components/home-page/discover-properties/discover-properties.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import DiscoverPropertiesCard from './discover-properties-card';
 import "./discover-properties.scss";
+import { getPopularAdverts } from '@/services/discover-service';
 
-const DiscoverProperties = () => {
-  return (
+const DiscoverProperties = async() => {  
+	const res = await getPopularAdverts();
+
+	const data = await res.json();
+  
+	console.log("DATA", data);
+  
+	if (!res.ok) throw new Error(data.message);
+	return (
+
+  
 		<div className="discover-properties-container">
 		 <h2 >Discover Popular Properties</h2>
       <p >Featured properties</p>
 			<div className="row g-4">
 					<div className="col-sm-6 col-md-4 col-lg-3" >
-						<DiscoverPropertiesCard />
+						<DiscoverPropertiesCard data={data}/>
 					</div>
 				
 			</div>
